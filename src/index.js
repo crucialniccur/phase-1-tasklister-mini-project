2337a62cf41b1b666f03e4f6bf8c127f0fdc3131
Mati Joseph <crucialniccur@gmail.com>
2025-03-16 19:10:13 +0300

    prevent default behaviour on the form submit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   // select my form
-  console.log(document.querySelector("#create-task-form"));
+  // console.log(document.querySelector("#create-task-form"));
+  console.log(document.querySelector("form"));
+
+  //prevent default behavour on form
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e);
+  });
 });
