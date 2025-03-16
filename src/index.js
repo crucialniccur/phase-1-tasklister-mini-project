e62528d9bad04e175004a96443cbfd270fb1e2c2
Mati Joseph <crucialniccur@gmail.com>
2025-03-16 19:32:04 +0300

    create a handleToDo function to handle the submit events

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
     // console.log(e);
-    console.log(e.target.new_task_description.value);
+    handleToDo(e.target.new_task_description.value);
   });
 });
+
+function handleToDo(todo) {
+  console.log(todo);
+}
