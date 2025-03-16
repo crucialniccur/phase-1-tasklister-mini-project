194fc2c0d180593f1c146cf6d4bb4d722a74aa60
Mati Joseph <crucialniccur@gmail.com>
2025-03-16 19:11:31 +0300

    log the target of the submitting action

--- src/index.js
+++ src/index.js
@@ -8,5 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(e);
+    console.log(e.target);
   });
 });
