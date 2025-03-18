37b0e111655197752e795da2a86bc86e440f7f77
Mati Joseph <crucialniccur@gmail.com>
2025-03-18 12:18:43 +0300

    prevent default behaviour on the form upon submission

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // prevent default
+  let form = document.querySelector("form");
+  console.log(form);
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("am working!!!!");
+  });
 });
